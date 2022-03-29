4f5af30c5e365468faea3e4d67e911597699b92e
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-03-29 12:48:13 +0000

    p7final.c

--- p0final.c
+++ p0final.c
@@ -1,45 +1,64 @@
 #include<stdio.h>
+#include<math.h>
 
-void input_string(char *string,char *substring)
+typedef struct _point 
 {
-  printf("Enter the string and substring:\n");
-  scanf("%s%s",string,substring);
+  float x,y;
+}Point;
+typedef struct _line
+{
+  Point p1,p2;
+  float distance;
+}Line;
+typedef struct _polygon
+{
+int n;
+Line l[100];
+float perimeter;
+}Polygon;
+int input_n()
+{
+  int n;
+  printf("Enter the number of sides:\n");
+  scanf("%d",&n);
+  return n;
+}
+Line input_line()
+{
+  Line t;
+  printf("Enter the point:\n");
+  scanf("%f %f %f %f",&t.p1.x,&t.p1.y,&t.p2.x,&t.p2.y);
+  t.distance=sqrt((t.p2.x - t.p1.x)*(t.p2.x - t.p1.x)+(t.p2.y - t.p1.y)*(t.p2.y - t.p1.y));
+  return t;
+}
+void input_n_lines(int n, Line l[n])
+{
+  for(int i=0;i<n;i++)
+    l[i]=input_line();
+}
+void input_polygon(int n,Polygon *p)
+{
+  p->n=n;
+  input_n_lines(p->n,p->l);
+  p->perimeter=0.0;
   
 }
-int str_reverse(char *string,char *substring)
+void find_perimeter(Polygon *p)
 {
-  int i,j;
-  for(i=0;string[i]!='\0';i++)
-    {
-      j=0;
-      while(string[i]==substring[j])
-        {
-          j++;
-          i++;
-          if(substring[j]=='\0')
-            return i-j;
-          if(string[i]!=substring[j]){
-            i--;
-            break;
-          }
-        }
-    }
-  return -1;
-  
+  for(int i=0;i<p->n;i++)
+    p->perimeter+=p->l[i].distance;
 }
-void output(char *string,char *substring,int reverse)
+void output(Polygon p)
 {
-  if(reverse>=0)
-    printf("The reverse of %s in %s is %d.\n",substring,string,reverse);
-  else
-    printf("The given substring %s is not present in %s.\n",substring,string);
+  printf("The perimeter of the polygon with %d sides is %f",p.n,p.perimeter);
   
 }
 int main()
 {
-  char string[20],substring[20];int x;
-  input_string(string,substring);
-  x=str_reverse(string,substring);
-  output(string,substring,x);
+  int x;Polygon p;
+  x=input_n();
+  input_polygon(x,&p);
+  find_perimeter(&p);
+  output(p);
   return 0;
 }
